174f830f1bf1e4b761c3d1ced7fe4c8feef754dc
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-04-18 21:43:23 +0200

    Added a bit of angular, new tasks are displaying in 'to do panel'

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module("app", []);
+app.controller("appCtrl", ["$scope", "tasks", function($scope, tasks) {
+    $scope.tasks = tasks;
+    $scope.addTask = function () {
+        $scope.tasks.push({"text": $scope.newTask.text, "date": inputDate.value, "done": false});
+        $scope.newTask.text = "";
+        $scope.newTask.date = moment().format("MM/DD/YYYY hh:mm A");
+    };
+   
+}]);
+app.factory('tasks', function () {
+        return [
+            {
+                "text": 'Find a job', "date": "30/04/2018 12:00 AM" ,"done": false,
+            }
+        ];
+    });
+const inputDate = document.getElementById("date");
+inputDate.addEventListener("keydown", function(event) {
+    event.preventDefault()
+});
