33994e11d13f239f8397fa4ba07d1f51b8c441e8
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-04-19 22:59:05 +0200

    Added new default tasks, removing done tasks function added,counter of left tasks, slight changes in styles - app done v1.0

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
 var app = angular.module("app", []);
 app.controller("appCtrl", ["$scope", "tasks", function($scope, tasks) {
     $scope.tasks = tasks;
+    $scope.left = function() {
+      var counter = 0;
+      angular.forEach($scope.tasks, function(task) {
+        counter += task.done ? 0 : 1;
+      });
+      return counter;
+    };
     $scope.addTask = function () {
         $scope.tasks.push({"text": $scope.newTask.text, "date": inputDate.value, "done": false});
         $scope.newTask.text = "";
         $scope.newTask.date = moment().format("MM/DD/YYYY h:mm A");
     };
-   
+    $scope.cleanDone = function () {
+        $scope.tasks = $scope.tasks.filter(function (task) {
+                return !task.done
+        })
+    };
 }]);
 app.factory('tasks', function () {
-        return [
-            {
-                "text": 'Find a job', "date": "30/04/2018 12:00 AM" ,"done": false,
-            }
-        ];
-    });
+    return [
+        {
+            "text": "Find a job", "date": "04/30/2018 12:00 AM", "done": false,
+        },
+        {
+            "text": "Meet with friends", "date": "04/27/2018 7:00 PM", "done": false,
+        },
+        {
+            "text": "Make 1.000.000th in the world todolist app", "date": "04/20/2018 2:00 PM", "done": true,
+        },
+    ];
+});
 const inputDate = document.getElementById("date");
 inputDate.addEventListener("keydown", function(event) {
     event.preventDefault()
 });
+
